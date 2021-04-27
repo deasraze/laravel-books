a3b94621def38374636f2ef831efe1707ffab8c6
theifel <condecrom@gmail.com>
2021-04-27 22:55:59 +0300

    Added admin panel views

---
+++ resources/views/admin/authors/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('admin.authors._nav')
+
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Date of birth</th>
+            <th>Number of books</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+
+        </tbody>
+    </table>
+@endsection
---
+++ resources/views/admin/books/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('admin.books._nav')
+
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Authors</th>
+            <th>Publishing house</th>
+            <th>Publication date</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+
+        </tbody>
+    </table>
+@endsection
