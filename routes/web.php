7e6b9e5bd51c6eb186a44ac48e00b05a1fe56716
theifel <condecrom@gmail.com>
2021-04-27 22:41:23 +0300

    Added BookController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Books\BookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', [BookController::class, 'index'])->name('home');
 
 Auth::routes();
