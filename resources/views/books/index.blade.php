5dbd9485733a794af15355e37970847d3b8e8d55
theifel <condecrom@gmail.com>
2021-04-27 22:40:52 +0300

    Added books views

---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('books._nav')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="items-list">
+                <div class="item">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div style="height: 300px; background: #f6f6f6; border: 1px solid #ddd"></div>
+                        </div>
+                        <div class="col-md-9">
+                            <span class="float-right"></span>
+                            <div class="h4" style="margin-top: 0">
+                                <a href="#"></a>
+                            </div>
+                            <p>Title: <a href=""></a></p>
+                            <p>Authors: <a href=""></a></p>
+                            <p>Publishing house: </p>
+                            <p>Publication date: </p>
+                            <p>Description: </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div style="height: 300px; background: #f6f6f6; border: 1px solid #ddd"></div>
+                        </div>
+                        <div class="col-md-9">
+                            <span class="float-right"></span>
+                            <div class="h4" style="margin-top: 0">
+                                <a href="#"></a>
+                            </div>
+                            <p>Title: <a href=""></a></p>
+                            <p>Authors: <a href=""></a></p>
+                            <p>Publishing house: </p>
+                            <p>Publication date: </p>
+                            <p>Description: </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div style="height: 300px; background: #f6f6f6; border: 1px solid #ddd"></div>
+                        </div>
+                        <div class="col-md-9">
+                            <span class="float-right"></span>
+                            <div class="h4" style="margin-top: 0">
+                                <a href="#"></a>
+                            </div>
+                            <p>Title: <a href=""></a></p>
+                            <p>Authors: <a href=""></a></p>
+                            <p>Publishing house: </p>
+                            <p>Publication date: </p>
+                            <p>Description: </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
