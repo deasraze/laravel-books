919d0d29b8100d5f1e8b81193335f9ebb0df0ca9
theifel <condecrom@gmail.com>
2021-04-27 20:24:54 +0300

    Changed webpack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+mix
+    .setPublicPath('public/build')
+    .setResourceRoot('/build/')
+    .js('resources/js/app.js', 'js')
+    .sass('resources/sass/app.scss', 'css');
+
+if (mix.inProduction()) {
+    mix.version();
+}
