fa1ac6044bd123d7a33c6d55f8d222b7284d0557
theifel <condecrom@gmail.com>
2021-04-27 22:40:41 +0300

    Added authors views

---
+++ resources/views/authors/index.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('authors._nav')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="items-list">
+                <div class="item">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div style="height: 300px; background: #f6f6f6; border: 1px solid #ddd"></div>
+                        </div>
+                        <div class="col-md-9">
+                            <span class="float-right"></span>
+                            <div class="h4" style="margin-top: 0">
+                                <a href="#"></a>
+                            </div>
+                            <p>Name: <a href=""></a></p>
+                            <p>Date of birth:</p>
+                            <p>Number of books: </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div style="height: 300px; background: #f6f6f6; border: 1px solid #ddd"></div>
+                        </div>
+                        <div class="col-md-9">
+                            <span class="float-right"></span>
+                            <div class="h4" style="margin-top: 0">
+                                <a href="#"></a>
+                            </div>
+                            <p>Name: <a href=""></a></p>
+                            <p>Date of birth:</p>
+                            <p>Number of books: </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div style="height: 300px; background: #f6f6f6; border: 1px solid #ddd"></div>
+                        </div>
+                        <div class="col-md-9">
+                            <span class="float-right"></span>
+                            <div class="h4" style="margin-top: 0">
+                                <a href="#"></a>
+                            </div>
+                            <p>Name: <a href=""></a></p>
+                            <p>Date of birth:</p>
+                            <p>Number of books: </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
