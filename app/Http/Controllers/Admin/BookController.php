578b3ba10d8f0c0cce6a2ff922bb50b2c5ccde9b
theifel <condecrom@gmail.com>
2021-04-28 22:05:26 +0300

    Updated BookController for admin panel

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -3,45 +3,60 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Books\BookRequest;
+use App\Models\Author;
 use App\Models\Book;
-use Illuminate\Http\Request;
+use App\UseCases\Books\BookService;
 
 class BookController extends Controller
 {
+    private BookService $service;
+
+    public function __construct(BookService $service)
+    {
+        $this->service = $service;
+    }
+
     public function index()
     {
-        $books = Book::orderByDesc('id')->paginate(10);
+        $books = Book::with('authors')->orderByDesc('id')->paginate(10);
 
         return view('admin.books.index', compact('books'));
     }
 
     public function create()
     {
-        //
+        $authors = Author::orderBy('last_name')->get();
+
+        return view('admin.books.create', compact('authors'));
     }
 
-    public function store(Request $request)
+    public function store(BookRequest $request)
     {
-        //
+        $book = $this->service->create($request);
+
+        return redirect()->route('books.show', $book);
     }
 
-    public function show($id)
+    public function edit(Book $book)
     {
-        //
+        $authors = Author::orderBy('last_name')->get();
+        $bookAuthors = $book->authors()->pluck('id')->toArray();
+
+        return view('admin.books.edit', compact('book', 'authors', 'bookAuthors'));
     }
 
-    public function edit($id)
+    public function update(BookRequest $request, Book $book)
     {
-        //
+        $this->service->edit($request, $book->id);
+
+        return redirect()->route('books.show', $book);
     }
 
-    public function update(Request $request, $id)
+    public function destroy(Book $book)
     {
-        //
-    }
+        $this->service->remove($book->id);
 
-    public function destroy($id)
-    {
-        //
+        return redirect()->route('admin.books.index');
     }
 }
