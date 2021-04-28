37fea834880d14fa2ce59ef485b35aa2cbfceff2
theifel <condecrom@gmail.com>
2021-04-28 12:09:02 +0300

    Small fix view create

--- resources/views/admin/books/create.blade.php
+++ resources/views/admin/books/create.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('admin.authors._nav')
-    <form method="POST" action="{{ route('admin.authors.store') }}">
+    @include('admin.books._nav')
+    <form method="POST" action="{{ route('admin.books.store') }}">
         @csrf
         <div class="form-group">
             <label for="title">Title</label>
@@ -21,30 +21,29 @@
             @enderror
         </div>
         <div class="form-group">
-            <label for="description">Year of birth</label>
-            <input class="form-control @error('description') is-invalid @enderror" type="date"
-                   name="description" id="description" value="{{ old('description') }}" required>
-            @error('description')
+            <label for="publish_date">Publication date</label>
+            <input class="form-control @error('publish_date') is-invalid @enderror" type="date"
+                   name="publish_date" id="publish_date" value="{{ old('publish_date') }}" required>
+            @error('publish_date')
             <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
             @enderror
         </div>
         <div class="form-group">
             <label for="authors">Authors</label>
-            <select class="custom-select @error('authors') is-invalid @enderror" name="authors[]" id="authors" multiple>
+            <select class="custom-select @error('authors.*') is-invalid @enderror"
+                    name="authors[]" id="authors" required multiple>
                 <option value="1">One</option>
                 <option value="2">Two</option>
                 <option value="3">Three</option>
             </select>
-            @error('authors')
+            @error('authors.*')
             <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
             @enderror
         </div>
         <div class="form-group">
             <label for="description">Description</label>
             <textarea class="form-control @error('description') is-invalid @enderror"
-                      id="description" name="description" rows="4">
-                {{ old('description') }}
-            </textarea>
+                      id="description" name="description" rows="4">{{ old('description') }}</textarea>
             @error('description')
             <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
             @enderror
