76dc48925495817a593c411278f6e780cc1b35d1
theifel <condecrom@gmail.com>
2021-04-28 22:30:58 +0300

    Changed index view

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -5,61 +5,32 @@
     <div class="row">
         <div class="col-md-12">
             <div class="items-list">
-                <div class="item">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <div style="height: 300px; background: #f6f6f6; border: 1px solid #ddd"></div>
-                        </div>
-                        <div class="col-md-9">
-                            <span class="float-right"></span>
-                            <div class="h4" style="margin-top: 0">
-                                <a href="#"></a>
+                @foreach($books as $book)
+                    <div class="item">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <div style="height: 300px; background: #f6f6f6; border: 1px solid #ddd"></div>
                             </div>
-                            <p>Title: <a href=""></a></p>
-                            <p>Authors: <a href=""></a></p>
-                            <p>Publishing house: </p>
-                            <p>Publication date: </p>
-                            <p>Description: </p>
+                            <div class="col-md-9">
+                                <span class="float-right"></span>
+                                <div class="h4" style="margin-top: 0">
+                                    <a href="{{ route('books.show', $book) }}">{{ $book->title }}</a>
+                                </div>
+                                <p>Authors:
+                                @foreach($book->authors as $author)
+                                     <a href="{{ route('authors.show', $author) }}">{{ $author->full_name }}</a>
+                                @endforeach
+                                </p>
+                                <p>Publishing house: {{ $book->publishing }}</p>
+                                <p>Publication date: {{ $book->publish_date }}</p>
+                                <p>Description: {{ substr($book->description, 0, 150) . '..' }}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="item">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <div style="height: 300px; background: #f6f6f6; border: 1px solid #ddd"></div>
-                        </div>
-                        <div class="col-md-9">
-                            <span class="float-right"></span>
-                            <div class="h4" style="margin-top: 0">
-                                <a href="#"></a>
-                            </div>
-                            <p>Title: <a href=""></a></p>
-                            <p>Authors: <a href=""></a></p>
-                            <p>Publishing house: </p>
-                            <p>Publication date: </p>
-                            <p>Description: </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <div style="height: 300px; background: #f6f6f6; border: 1px solid #ddd"></div>
-                        </div>
-                        <div class="col-md-9">
-                            <span class="float-right"></span>
-                            <div class="h4" style="margin-top: 0">
-                                <a href="#"></a>
-                            </div>
-                            <p>Title: <a href=""></a></p>
-                            <p>Authors: <a href=""></a></p>
-                            <p>Publishing house: </p>
-                            <p>Publication date: </p>
-                            <p>Description: </p>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
+
+    {{ $books->links() }}
 @endsection
