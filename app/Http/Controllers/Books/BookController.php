6964984f1a102dfa5e34e93ee02a4bc887a3335a
theifel <condecrom@gmail.com>
2021-04-28 22:31:06 +0300

    Updated BookController

--- app/Http/Controllers/Books/BookController.php
+++ app/Http/Controllers/Books/BookController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Books;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Book;
 
 class BookController extends Controller
 {
     public function index()
     {
-        return view('books.index');
+        $books = Book::with('authors')->orderByDesc('id')->paginate(10);
+
+        return view('books.index', compact('books'));
+    }
+
+    public function show(Book $book)
+    {
+        $authors = $book->authors()->orderBy('last_name')->get();
+
+        return view('books.show', compact('book', 'authors'));
     }
 }
