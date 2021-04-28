2f42d42e18afb5852cbc1b40abbe7616f382b4c7
theifel <condecrom@gmail.com>
2021-04-28 22:48:53 +0300

    Updated views

--- resources/views/authors/index.blade.php
+++ resources/views/authors/index.blade.php
@@ -5,55 +5,26 @@
     <div class="row">
         <div class="col-md-12">
             <div class="items-list">
-                <div class="item">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <div style="height: 300px; background: #f6f6f6; border: 1px solid #ddd"></div>
-                        </div>
-                        <div class="col-md-9">
-                            <span class="float-right"></span>
-                            <div class="h4" style="margin-top: 0">
-                                <a href="#"></a>
+                @foreach($authors as $author)
+                    <div class="item">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <div style="height: 300px; background: #f6f6f6; border: 1px solid #ddd"></div>
                             </div>
-                            <p>Name: <a href=""></a></p>
-                            <p>Date of birth:</p>
-                            <p>Number of books: </p>
+                            <div class="col-md-9">
+                                <span class="float-right"></span>
+                                <div class="h4" style="margin-top: 0">
+                                    <a href="{{ route('authors.show', $author) }}">{{ $author->full_name }}</a>
+                                </div>
+                                <p>Date of birth: {{ $author->birth_year }}</p>
+                                <p>Number of books: {{ $author->books_count }}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="item">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <div style="height: 300px; background: #f6f6f6; border: 1px solid #ddd"></div>
-                        </div>
-                        <div class="col-md-9">
-                            <span class="float-right"></span>
-                            <div class="h4" style="margin-top: 0">
-                                <a href="#"></a>
-                            </div>
-                            <p>Name: <a href=""></a></p>
-                            <p>Date of birth:</p>
-                            <p>Number of books: </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <div style="height: 300px; background: #f6f6f6; border: 1px solid #ddd"></div>
-                        </div>
-                        <div class="col-md-9">
-                            <span class="float-right"></span>
-                            <div class="h4" style="margin-top: 0">
-                                <a href="#"></a>
-                            </div>
-                            <p>Name: <a href=""></a></p>
-                            <p>Date of birth:</p>
-                            <p>Number of books: </p>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
+
+    {{ $authors->links() }}
 @endsection
