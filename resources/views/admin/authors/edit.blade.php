5738e47746d4428508eb1167a8c5a5f328887b43
theifel <condecrom@gmail.com>
2021-04-28 14:40:36 +0300

    Added edit views

---
+++ resources/views/admin/authors/edit.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('admin.authors._nav')
+    <form method="POST" action="{{ route('admin.authors.update') }}">
+        @csrf
+        @method('PUT')
+        <div class="form-group">
+            <label for="first_name">First name</label>
+            <input class="form-control @error('first_name') is-invalid @enderror"
+                   name="first_name" id="first_name" value="{{ old('first_name', $author->first_name) }}" required>
+            @error('first_name')
+            <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="last_name">Last name</label>
+            <input class="form-control @error('last_name') is-invalid @enderror"
+                   name="last_name" id="last_name" value="{{ old('last_name', $author->last_name) }}" required>
+            @error('last_name')
+            <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="birth_year">Year of birth</label>
+            <input class="form-control @error('birth_year') is-invalid @enderror" type="date"
+                   name="birth_year" id="birth_year" value="{{ old('birth_year', $author->birth_year) }}" required>
+            @error('birth_year')
+            <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
+            @enderror
+        </div>
+        <button type="submit" class="btn btn-primary">Save</button>
+    </form>
+@endsection
---
+++ resources/views/admin/books/edit.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('admin.books._nav')
+    <form method="POST" action="{{ route('admin.books.update') }}">
+        @csrf
+        @method('PUT')
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input class="form-control @error('title') is-invalid @enderror"
+                   name="title" id="title" value="{{ old('title', $book->title) }}" required>
+            @error('title')
+            <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="publishing">Publishing house</label>
+            <input class="form-control @error('publishing') is-invalid @enderror"
+                   name="publishing" id="publishing" value="{{ old('publishing', $book->publishing) }}" required>
+            @error('publishing')
+            <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="publish_date">Publication date</label>
+            <input class="form-control @error('publish_date') is-invalid @enderror" type="date"
+                   name="publish_date" id="publish_date" value="{{ old('publish_date', $book->publish_date) }}" required>
+            @error('publish_date')
+            <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="authors">Authors</label>
+            <select class="custom-select @error('authors.*') is-invalid @enderror"
+                    name="authors[]" id="authors" required multiple>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+            @error('authors.*')
+            <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea class="form-control @error('description') is-invalid @enderror"
+                      id="description" name="description" rows="4">{{ old('description', $book->description) }}</textarea>
+            @error('description')
+            <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
+            @enderror
+        </div>
+        <button type="submit" class="btn btn-primary">Save</button>
+    </form>
+@endsection
