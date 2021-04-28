6c93ef2ae4bed05ce7cfc3510db6c3fc4547f829
theifel <condecrom@gmail.com>
2021-04-28 08:20:22 +0300

    Added getFullName method

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -5,7 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $birth_year
+ */
 class Author extends Model
 {
     use HasFactory;
+
+    public function getFullName(): string
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
 }
